765444224b57cbe11ef1d74debf8f2a9d9eaa520
Raul Carlomagno <rcarlomagno@gmail.com>
2018-07-24 18:19:47 -0300

    remove local rules cache

--- mlprx-svr/index.js
+++ mlprx-svr/index.js
@@ -1,10 +1,7 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
 const winston = require('winston');
-
-const Catbox = require('catbox');
-const CatboxMemory = require('catbox-memory'); //se podria usar un redis local para acelerar la performance, no el redis distribuido
-const cache = new Catbox.Client(CatboxMemory);
+const Promise = require("bluebird");
 
 
 var Redis = require('ioredis');
@@ -17,6 +14,7 @@ var proxy = httpProxy.createProxyServer({});
 
 //TokenBucket
 //RateLimiter
+//se podria usar reglas locales en memoria para no ir a redis a buscarlas cada vez q llega un request
 
 var processCounterScript = fs.readFileSync('process_counter.lua').toString(); //deberimos cargarlo con loadscript desde el manager y aca ejecutar un evalsha con el hash qeu vendria por config
 
@@ -24,6 +22,36 @@ redis.defineCommand('processCounter', {
     numberOfKeys: 1,
     lua: processCounterScript
 });
+
+function secondsToStr(seconds) {
+
+    function numberEnding (number) {
+        return (number > 1) ? 's' : '';
+    }
+
+    var years = Math.floor(seconds / 31536000);
+    if (years) {
+        return years + ' year' + numberEnding(years);
+    }
+    //TODO: Months! Maybe weeks? 
+    var days = Math.floor((seconds %= 31536000) / 86400);
+    if (days) {
+        return days + ' day' + numberEnding(days);
+    }
+    var hours = Math.floor((seconds %= 86400) / 3600);
+    if (hours) {
+        return hours + ' hour' + numberEnding(hours);
+    }
+    var minutes = Math.floor((seconds %= 3600) / 60);
+    if (minutes) {
+        return minutes + ' minute' + numberEnding(minutes);
+    }
+    var seconds = seconds % 60;
+    if (seconds) {
+        return seconds + ' second' + numberEnding(seconds);
+    }
+    return 'less than a second';
+}
 
 /*
 fs.watch('clean.lua', () => {
@@ -58,49 +86,65 @@ proxy.on('error', (err, req, res) => {
     res.end('Something went wrong. And we are reporting a custom error message.');    
 });
 
+
+redis.monitor(function (err, monitor) {
+    monitor.on('monitor', function (time, args, source, database) {
+        console.log(args);
+    });
+});
+
 var server = http.createServer(async (req, res) => {
     const clientIp = (req.connection.remoteAddress == '::1' ? '127.0.0.1' : req.connection.remoteAddress).replace(/:/g, '·'); //sino redis toma los : como separador del key
-    const luaIpKey = 'ip:' + clientIp;
-    const luaPathKey = 'path:' + req.url;
-    const luaIpPathKey = 'ip-path:' + clientIp + '-' + req.url;
+    const luaIpKey = 'state:ip:' + clientIp;
+    const luaPathKey = 'state:path:' + req.url;
+    const luaIpPathKey = 'state:ip-path:' + clientIp + '-' + req.url;
 
     var epochSeconds =  Date.now()/1000|00;
-    var expiresInSeconds = 5;
-    const MAX_QTY_IN_WINDOW = 2;
 
     console.log("requesting %s from ip %s", req.url, clientIp);
 
-    //redis.eval(processCounterScript, 1, luaIpKey, epochSeconds, expiresInSeconds, MAX_QTY_IN_WINDOW).then(result => {
+    var rulesPromises = [
+        () => redis.get("rules:ip:" + clientIp),
+        () => redis.get("rules:ip:*"),
+        () => redis.get("rules:path:" + req.url),
+        () => redis.get("rules:ip-path:" + clientIp + ":" + req.url)
+    ];   
 
-    redis.processCounter(luaPathKey, epochSeconds, 10, 5);
-    redis.processCounter(luaIpPathKey, epochSeconds, 5, 2);
+    Promise.each(rulesPromises, (rulePromise, index, length) => {
+        rulePromise().then(rule => {
+            if(rule){
+                rule = JSON.parse(rule);
 
-    var rule = await cache.get({segment: 'rules', id: clientIp});
-    if(rule) rule = rule.item;
-
-    redis.processCounter(luaIpKey, epochSeconds, rule.timeWindow, rule.maxHits).then(result => {
-        var rateLimitReached = result[0] == 1;
-
-        //console.log(result);
-       
-
-        if(rateLimitReached) { //denied
-            res.writeHead(429, { 'Content-Type': 'text/plain' });
-            let limitMsg = `you have reach your limit of ${MAX_QTY_IN_WINDOW} hits in the last ${expiresInSeconds} seconds`;
-            console.warn(limitMsg);
-            if(result[3] != 0) {//oldest item
-                //console.error("please retry in %s seconds", new Date((result[3] + expiresInSeconds)*1000));
-                let v1 = new Date(epochSeconds*1000);
-                let v2 = new Date((result[3]+ expiresInSeconds)*1000);
-                console.error("please retry in %s seconds", (v2 - v1)/1000);
+                redis.processCounter(luaIpKey, epochSeconds, rule.timeWindow, rule.maxHits).then(result => {
+                    var rateLimitReached = result[0] == 1;
+            
+                    //console.log(result);
+                   
+            
+                    if(rateLimitReached) { //denied
+                        res.writeHead(429, { 'Content-Type': 'text/plain' });
+                        let limitMsg = `you have reach your limit of ${rule.maxHits} hits in the last ${rule.timeWindow} seconds`;
+                        console.warn(limitMsg);
+                        if(result[3] != 0) {//oldest item
+                            //console.error("please retry in %s seconds", new Date((result[3] + expiresInSeconds)*1000));
+                            let v1 = new Date(epochSeconds*1000);
+                            let v2 = new Date((result[3]+ rule.timeWindow)*1000);
+                            console.error("please retry in %s", secondsToStr((v2 - v1)/1000));
+                        }
+                        res.end(limitMsg);
+                    } else {
+                        res.writeHead(200, { 'Content-Type': 'text/plain' });
+                        res.end('request successfully proxied to: ' + req.url);
+                    }
+            
+                });   
             }
-            res.end(limitMsg);
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end('request successfully proxied to: ' + req.url);
-        }
-
+                return Promise.resolve();
+        });
     });
+    
+
+
 
     
  
@@ -120,8 +164,13 @@ var server = http.createServer(async (req, res) => {
 });
 
 
-(async() =>  {
-    await cache.start();
-    await cache.set({segment: 'rules', id:'127.0.0.1'}, {maxHits: 2, timeWindow: 5});
+(() =>  {
+   
+    redis.set("rules:ip:127.0.0.1", JSON.stringify({maxHits: 1, timeWindow: 2})); //se podria haber usado un hset para optimizar un poco mas, pero se harian mas lecturas a redis para recuperar y setear
+    redis.set("rules:ip:*", JSON.stringify({maxHits: 2, timeWindow: 10})); 
+    redis.set("rules:path:/sites/MLA/categories", JSON.stringify({maxHits: 1, timeWindow: 10}));
+    redis.set("rules:ip-path:*-/sites/MLB/categories", JSON.stringify({maxHits: 2, timeWindow: 4}));
+
+
     server.listen(PORT, () => console.log("listening on port %s", PORT));
 })() 
